28df2acaf0500873f5b180aa269444b242b9f06a
dario.bocale@itc-studenten.de <dario.bocale@itc-studenten.de>
2023-09-04 10:25:14 +0200

    - Anleitung erweitert

--- src/main/java/general/General.java
+++ src/main/java/general/General.java
@@ -7,13 +7,14 @@ public class General {
     public void zeigeAnleitung(MessageCreateEvent event) {
         event.getChannel().sendMessage("""
                 Hier findest Du eine Anleitung zu den atkuell integrierten Befehlen:
+                **Alle Befehle sind nicht Case-sensitiv, können also groß, klein oder wie auch immer geschrieben werden.**
                 - **!Hallo** (Bottas wird dich zurück grüßen!)
-                - **!poll Frage? Antwort! Antwort! ...** So kann eine Umfrage gestartet werden. Es können bis zu 10 Antworten angegeben werden.
+                - **!Poll Frage? Antwort! Antwort! ...** So kann eine Umfrage gestartet werden. Es können bis zu 10 Antworten angegeben werden.
                         Beispiel: !poll Ist der Bot nicht geil? Ja! Juicy ist geiler! Ich mag Züge!
-                - **!del x** (löscht die letzten x Nachrichten eines Channels. Für "x" kann eine beliebige Zahl eingesetzt werden.)
-                - **!mute** (mutet alle Teilnehmer des Voicechannels, in dem sich der Autor des Befehls befindet)
-                - **!unmute** (unmutet alle Teilnehmer des Voicechannels, in dem sich der Autor des Befehls befindet)
-                - **!play x** (Bottas tritt dem Channel bei und spielt Musik ab. An der Stelle von x muss z.B. ein Youtubelink eingefügt werden.
+                - **!Del x** (löscht die letzten x Nachrichten eines Channels. Für "x" kann eine beliebige Zahl eingesetzt werden.)
+                - **!MuteAll** (mutet alle Teilnehmer des Voicechannels, in dem sich der Autor des Befehls befindet)
+                - **!UnmuteAll** (unmutet alle Teilnehmer des Voicechannels, in dem sich der Autor des Befehls befindet)
+                - **!Play x** (Bottas tritt dem Channel bei und spielt Musik ab. An der Stelle von x muss z.B. ein Youtubelink eingefügt werden.
                         Erlaubte Formate sind:
                         YouTube
                         SoundCloud
@@ -24,7 +25,10 @@ public class General {
                         HTTP URLs
                         (Playlists funktionieren nicht)
                         (Eine Track Queue ist in Arbeit)
-                - **!stop** (stoppt die Musikwiedergabe von Bottas sofort und disconnected ihn vom Channel)"""
+                - **!Stop** (stoppt die Musikwiedergabe von Bottas sofort und disconnected ihn vom Channel)
+                
+                **Viel Spaß mit Bottas!**
+                """
         );
     }
 
